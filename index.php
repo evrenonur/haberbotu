05b939ee776c14215b5d43a0664eb8b01018a175
Onur Evren <onur.evren.1999@gmail.com>
2022-09-09 21:35:31 +0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "vendor/autoload.php";
-
-$sondakika = new \Evren\Haberler\Siteler\SonDakikaCom();
-echo "<pre>";
-print_r($sondakika->getCategoryNews(\Evren\Haberler\Kategoriler\SonDakikaComKategoriler::DUNYA));
-echo "</pre>";
